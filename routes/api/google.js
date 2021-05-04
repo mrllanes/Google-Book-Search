3e525f9c8c93ef2d811c8f990535b9cafdfbd6a5
Mr. Llanes <llanesm@gmail.com>
2021-05-04 18:31:43 -0500

    Created routes for google and books and using an index for them.

---
+++ routes/api/google.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/:title", (req, res) => {
+    axios
+        .get(
+            "https://www.googleapis.com/books/v1/volumes?q=" + req.params.title
+        )
+        .then((results) => {
+            console.log(reults);
+            res.json(results);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const bookRoutes = require("./books");
+const googleRoutes = require("./google");
+
+router.use("/books", bookRoutes);
+router.use("/google", googleRoutes);
+
+module.exports = router;
