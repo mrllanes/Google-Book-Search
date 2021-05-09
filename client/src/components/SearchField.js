4dab24bf93110b65950f3dbbf35d48bb56e6e60b
Mr. Llanes <llanesm@gmail.com>
2021-05-09 03:20:52 -0500

    Attemp to add Search component.

---
+++ client/src/components/SearchField.js
@@ -0,0 +1,13 @@
+export const SearchField = () => {
+    return (
+        <div className="input-group mb-3">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Search for a Book!"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+            />
+        </div>
+    );
+};
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import API from "../utils/API";
-import BookCard from "../components/BookCard";
+import { BookCard } from "../components/BookCard";
+import { SearchField } from "../components/SearchField";
 
 export const Search = () => {
     const [books, setBooks] = useState([]);
@@ -8,14 +9,41 @@ export const Search = () => {
     const [message, setMessage] = useState("Search for Books Here");
 
     const searchBooks = () => {
-        API.searchBooks(searchWord).then((results) => {
-            console.log(results);
-        });
+        API.searchBooks(searchWord)
+            .then((results) => {
+                console.log(results);
+                setBooks(results.data);
+            })
+            .catch(() => {
+                setBooks([]);
+                setMessage("Nothing to see here, try again!");
+            });
     };
 
     return (
-        <div className="container text-center">
-            <div className="row"></div>
-        </div>
+        <>
+            <div className="container text-center">
+                <SearchField />
+                <button
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                    onClick={searchBooks}
+                >
+                    Search
+                </button>
+            </div>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        {books.map((data) => (
+                            <div key={data.id} classname="col">
+                                <BookCard {...data} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
